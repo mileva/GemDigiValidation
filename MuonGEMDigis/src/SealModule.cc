ffbba223d6f8b4e42152f95bbb9499213d7159ec
Roumyana <roumyana.mileva.hadjiiska@cern.ch>
2013-10-13 13:35:05 +0200

    Initial commit

---
+++ MuonGEMDigis/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "myValidation/MuonGEMDigis/interface/GEMDigiValid.h"
+
+
+DEFINE_FWK_MODULE(GEMDigiValid);
